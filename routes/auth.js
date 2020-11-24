7ce86c2c098d357ee69e41bdd1bcc546ab4cbd67
Astral1004 <kambulov18@yandex.ru>
2020-11-24 10:36:49 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('loginpage', {
+  res.render('login', {
     layout: 'empty',
     title: 'Авторизация',
   });
