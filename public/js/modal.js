2b4b8eebfd18d6314849f73cb3309ad0fad6bd65
MrHalless <mrhalless48@yandex.ru>
2020-12-02 17:16:22 +0300

    V 2.0
    Довели в порядок файлы

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,123 +1,60 @@
-<<<<<<< Updated upstream
-// const { openModal } = require('../js/app');
+// // const { openModal } = require('../js/app');
 
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
 
-var btn = document.getElementById('myBtn');
+// var btn = document.getElementById('myBtn');
 
-let activeUser = document.getElementById('activeUser');
-// var span = document.getElementsByClassName('close')[0];
+// let activeUser = document.getElementById('activeUser');
+// // var span = document.getElementsByClassName('close')[0];
 
-/* function qwerty() {
-    modal.style.display = "block";
-}
- */
+// /* function qwerty() {
+//     modal.style.display = "block";
+// }
+//  */
 
-// span.onclick = function () {
-//   modal.style.display = 'none';
+// // span.onclick = function () {
+// //   modal.style.display = 'none';
+// // };
+
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = 'none';
+//   }
 // };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+// let timeout = 3000;
+// let lastActiveTimestamp = 0;
+// let userIsActive = false;
 
-let timeout = 3000;
-let lastActiveTimestamp = 0;
-let userIsActive = false;
+// // window.addEventListener('mousemove', active);
+// window.addEventListener('keypress', active);
+// window.addEventListener('click', active);
 
-// window.addEventListener('mousemove', active);
-window.addEventListener('keypress', active);
-window.addEventListener('click', active);
+// // setInterval(checkUserIsActive, 2000);
+// // active();
 
-// setInterval(checkUserIsActive, 2000);
-// active();
+// function checkUserIsActive() {
+//   if (userIsActive && new Date().getTime() - lastActiveTimestamp > timeout) {
+//     modal.style.display = 'block';
+//     console.log('user is not active');
+//     // document.querySelector('div').textContent = 'user is inactive';
+//     userIsActive = false;
+//     setInterval(function () {
+//       modal.style.display = 'none';
+//       location.reload();
+//     }, 60000);
+//   }
+// }
 
-function checkUserIsActive() {
-  if (userIsActive && new Date().getTime() - lastActiveTimestamp > timeout) {
-    modal.style.display = 'block';
-    console.log('user is not active');
-    // document.querySelector('div').textContent = 'user is inactive';
-    userIsActive = false;
-    setInterval(function () {
-      modal.style.display = 'none';
-      location.reload();
-    }, 60000);
-  }
-}
+// function active() {
+//   lastActiveTimestamp = new Date().getTime();
+//   if (!userIsActive) {
+//     userIsActive = true;
+//     console.log('user is active');
+//     //document.querySelector('div').textContent = 'user is active';
+//   }
+// }
 
-function active() {
-  lastActiveTimestamp = new Date().getTime();
-  if (!userIsActive) {
-    userIsActive = true;
-    console.log('user is active');
-    //document.querySelector('div').textContent = 'user is active';
-  }
-}
-
-activeUser.addEventListener('click', () => {
-  location.reload();
-});
-=======
-// const { openModal } = require('../js/app');
-
-var modal = document.getElementById('myModal');
-
-var btn = document.getElementById('myBtn');
-
-let activeUser = document.getElementById('activeUser');
-// var span = document.getElementsByClassName('close')[0];
-
-/* function qwerty() {
-    modal.style.display = "block";
-}
- */
-
-// span.onclick = function () {
-//   modal.style.display = 'none';
-// };
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
-
-let timeout = 3000;
-let lastActiveTimestamp = 0;
-let userIsActive = false;
-
-// window.addEventListener('mousemove', active);
-window.addEventListener('keypress', active);
-window.addEventListener('click', active);
-
-// setInterval(checkUserIsActive, 2000);
-// active();
-
-function checkUserIsActive() {
-  if (userIsActive && new Date().getTime() - lastActiveTimestamp > timeout) {
-    modal.style.display = 'block';
-    console.log('user is not active');
-    // document.querySelector('div').textContent = 'user is inactive';
-    userIsActive = false;
-    setInterval(function () {
-      modal.style.display = 'none';
-      location.reload();
-    }, 60000);
-  }
-}
-
-function active() {
-  lastActiveTimestamp = new Date().getTime();
-  if (!userIsActive) {
-    userIsActive = true;
-    console.log('user is active');
-    //document.querySelector('div').textContent = 'user is active';
-  }
-}
-
-activeUser.addEventListener('click', () => {
-  location.reload();
-});
->>>>>>> Stashed changes
+// activeUser.addEventListener('click', () => {
+//   location.reload();
+// });
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
@@ -61,67 +60,3 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
-=======
-const { Router } = require('express');
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
-const checkAuth = require('../middleware/checkAuth');
-// const { db } = require('../models/user');
-// const { Mongoose } = require('mongoose');
-const router = Router();
-// const mongoose = require('mongoose');
-
-router.get('/', (req, res) => {
-  res.render('login', {
-    layout: 'empty',
-    title: 'Авторизация',
-    error: req.flash('error'),
-  });
-});
-
-router.get('/logout', checkAuth, async (req, res) => {
-  req.session.destroy(() => {
-    // db.sessions.remove(req.body.id);
-    //   // req.session.id.destroy();
-    //   // req.session.cookie.destroy();
-    res.redirect('/');
-  });
-});
-
-// ! авторизация
-router.post('/', async (req, res) => {
-  try {
-    const { login, password } = req.body;
-    const candidate = await User.findOne({ login });
-
-    if (candidate) {
-      const areSame = await bcrypt.compare(password, candidate.password);
-      if (areSame) {
-        req.session.user = candidate;
-        req.session.isAuthenticated = true;
-        req.session.settings = true;
-        if (candidate.typeUser === 'Pr') {
-          req.session.cookie.maxAge = null;
-        }
-        req.session.save((err) => {
-          if (err) {
-            throw err;
-          }
-          req.flash('success', 'Добро пожаловать');
-          res.redirect('/index');
-        });
-      } else {
-        req.flash('error', 'Ошибка аутентификации');
-        res.redirect('/');
-      }
-    } else {
-      req.flash('error', 'Ошибка аутентификации');
-      res.redirect('/');
-    }
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-module.exports = router;
->>>>>>> Stashed changes
